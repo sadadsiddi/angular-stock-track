65ae5460a192d085745d27e8df5f42f0e1ceb13f
sadadsiddi <sadaditf@gmail.com>
2022-08-10 17:21:30 +0530

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { Routes } from '@angular/router';
+import { StockTrackerService } from './stock-tracker.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SocialSentimentComponent } from './social-sentiment/social-sentiment.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'sentiment/:symbol', component: SocialSentimentComponent },
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent],
+  providers: [StockTrackerService],
+  exports: [RouterModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/social-sentiment/social-sentiment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-social-sentiment',
+  templateUrl: './social-sentiment.component.html',
+  styleUrls: ['./social-sentiment.component.css']
+})
+export class SocialSentimentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
